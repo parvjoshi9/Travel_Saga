844e8485681885d8348290254b9436a83e3c19d5
parvjoshi9 <76940332+parvjoshi9@users.noreply.github.com>
2022-09-17 10:13:38 +0530

    Delete src directory

--- src/Components/PlaceInfo/PlaceInfo.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const PlaceInfo = () => {
-  return <h1>PlaceInfo</h1>;
-};
-export default PlaceInfo;
--- src/Components/PlaceInfo/PlaceInfo.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PlaceInfo = () => {
-  return <h1>hi there</h1>;
-};
-
-export default PlaceInfo;
