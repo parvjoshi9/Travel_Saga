00ca3eb9212092714f71ae7b51c7b9af81521b0b
Parvcyber <parv9.joshi@gmail.com>
2022-09-17 09:54:08 +0530

    comment

---
+++ src/Components/PlaceInfo/PlaceInfo.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const PlaceInfo = () => {
+  return <h1>PlaceInfo</h1>;
+};
+export default PlaceInfo;
---
+++ src/Components/PlaceInfo/PlaceInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlaceInfo = () => {
+  return <h1>hi there</h1>;
+};
+
+export default PlaceInfo;
